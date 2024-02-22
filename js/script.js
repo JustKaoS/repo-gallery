b40ad771da091b0035183ac7ba4f74468cc0d1e1
Daniel Mott <daniel.r.mott@gmail.com>
2024-02-22 01:01:23 -0800

    Added API calls for user data

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 const username = `daniel-r-mott`;
 
+// Fetches Github user data for username
 const gitFetch = async function() {
    const request = await fetch(`https://api.github.com/users/${username}`);
    const data = await request.json();
    displayInfo(data);
-   console.log(data);
-}
+};
 
 gitFetch();
+
 
 const displayInfo = function(data) {
     const userInfo = document.createElement("div");
@@ -30,4 +32,22 @@ const displayInfo = function(data) {
         </div>
         `
         overview.append(userInfo);
+repoFetch();
 };
+
+// Fetches Github user repositories
+const repoFetch = async function() {
+    const request = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await request.json();
+    repoInfo(repoData);
+};
+
+
+const repoInfo = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}<h3>`;
+        repoList.append(repoItem);
+    }
+}
